a5435a4bef402269ec70b93e01a9fb97ab76fb74
lonequantum <lonequantum@lnode.net>
2021-09-26 13:01:24 +0200

    Add basic one-level completion for subcommands

--- kianidraw.bash
+++ kianidraw.bash
@@ -1,4 +1,23 @@
 #/usr/bin/env bash
 
 alias kian=kianidraw
-complete -W "new import edit update get set" kianidraw kian
+
+_kian_completion() {
+	case $COMP_CWORD in
+	1)
+		COMPREPLY=($(compgen -W "new import edit update get set" "${COMP_WORDS[1]}"));;
+	2)
+		case ${COMP_WORDS[1]} in
+		edit)
+			COMPREPLY=($(compgen -W "$(kianidraw-get resources 2>/dev/null)" "${COMP_WORDS[2]}"));;
+		update)
+			COMPREPLY=($(compgen -W "all $(kianidraw-get resources 2>/dev/null)" "${COMP_WORDS[2]}"));;
+		set)
+			COMPREPLY=($(compgen -W "config" "${COMP_WORDS[2]}"));;
+		get)
+			COMPREPLY=($(compgen -W "config resources" "${COMP_WORDS[2]}"));;
+		esac
+	esac
+}
+
+complete -F _kian_completion kianidraw kian
