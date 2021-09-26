b27a643f90e765aaa4bbf787476656daf7231d0d
lonequantum <lonequantum@lnode.net>
2021-09-26 15:19:02 +0200

    Finish completions for subcommands

--- kianidraw.bash
+++ kianidraw.bash
@@ -5,22 +5,73 @@ alias kian=kianidraw
 _kian_completion() {
 	case $COMP_CWORD in
 	1)
-		COMPREPLY=($(compgen -W "new import edit update get set" "${COMP_WORDS[1]}"));;
+		COMPREPLY=($(compgen -W "\
+			new\
+			import\
+			edit\
+			update\
+			get\
+			set\
+			"\
+			"${COMP_WORDS[1]}"\
+		));;
+
 	2)
 		case ${COMP_WORDS[1]} in
 		edit)
-			COMPREPLY=($(compgen -W "$(kianidraw-get resources 2>/dev/null)" "${COMP_WORDS[2]}"));;
+			COMPREPLY=($(compgen -W "\
+				$(kianidraw-get resources 2>/dev/null)\
+				"\
+				"${COMP_WORDS[2]}"\
+				));;
 		update)
-			COMPREPLY=($(compgen -W "all $(kianidraw-get resources 2>/dev/null)" "${COMP_WORDS[2]}"));;
+			COMPREPLY=($(compgen -W "\
+				all\
+				$(kianidraw-get resources 2>/dev/null)\
+				"\
+				"${COMP_WORDS[2]}"\
+			));;
 		set)
-			COMPREPLY=($(compgen -W "config" "${COMP_WORDS[2]}"));;
+			COMPREPLY=($(compgen -W "\
+				$(kianidraw-get config 2>/dev/null | cut -d = -f1 | awk '{print "config/"$0}')\
+				"\
+				"${COMP_WORDS[2]}"\
+			));;
 		get)
 			COMPREPLY=($(compgen -W "\
-				$(kianidraw-get config 2>/dev/null | cut -d = -f1 | awk '{print "config/"$0}') \
-				resources/all \
-				$(kianidraw-get resources 2>/dev/null | awk '{print "resources/"$0}') \
-				" \
-				"${COMP_WORDS[2]}" \
+				$(kianidraw-get config 2>/dev/null | cut -d = -f1 | awk '{print "config/"$0}')\
+				resources/all\
+				$(kianidraw-get resources 2>/dev/null | awk '{print "resources/"$0}')\
+				"\
+				"${COMP_WORDS[2]}"\
+			));;
+		esac;;
+
+	3)
+		case ${COMP_WORDS[1]} in
+		update)
+			COMPREPLY=($(compgen -W "\
+				all\
+				$(kianidraw-get resources 2>/dev/null)\
+				"\
+				"${COMP_WORDS[3]}"\
+			));;
+		set)
+			COMPREPLY=($(compgen -W "\
+				default\
+				"\
+				"${COMP_WORDS[3]}"\
+			));;
+		esac;;
+
+	*)
+		case ${COMP_WORDS[1]} in
+		update)
+			COMPREPLY=($(compgen -W "\
+				all\
+				$(kianidraw-get resources 2>/dev/null)\
+				"\
+				"${COMP_WORDS[$COMP_CWORD]}"\
 			));;
 		esac
 	esac
