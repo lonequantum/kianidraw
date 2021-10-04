9b42a440f97a0beaf082eb025f8c509b97bda8c9
lonequantum <lonequantum@lnode.net>
2021-10-04 15:43:38 +0200

    Rename extension of m4ized source files

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 
 for dir in lib bin; do
 	for file in src/$dir/kianidraw*; do
-		command="m4 $file > $(echo $file | sed s/src/target/ | sed s/\.m4//)"
+		command="m4 $file > $(echo $file | sed s/src/target/ | sed s/\.sh\.m4//)"
 		echo $command
 		eval $command
 	done
